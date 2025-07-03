0e22723bea6b5c8bcf1ee43d5a3c426069c228e1
Jay Vercellone <jay@pipedream.com>
2025-07-03 16:05:01 -0700

    Use new custom client

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * as Pipedream from "./api/index.js";
 export { PipedreamError, PipedreamTimeoutError } from "./errors/index.js";
-export { PipedreamClient } from "./Client.js";
 export { PipedreamEnvironment } from "./environments.js";
+export { Pipedream as PipedreamClient } from "./wrapper/Pipedream.js";
